7ba51492b7c64d7e212f1e16d03f3fabb8fef24c
jefffhaynes <feedyurhed@gmail.com>
2015-01-18 23:42:28 -0500

    new version of XBee with startup fix

--- NodeControl/NetworkController.cs
+++ NodeControl/NetworkController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO.Ports;
 using System.Threading.Tasks;
+using BinarySerialization;
 using SC18IM700;
 using SC18IM700.Commands;
 using XBee;
@@ -10,8 +11,12 @@ namespace NodeControl
 {
     public static class NetworkController
     {
-        private static XBeeController _xBee = new XBeeController();
+        private static XBeeController _xBee;
         private static bool _isInitialized;
+
+        public static event EventHandler InitializingController;
+
+        public static event EventHandler DiscoveringNetwork;
 
         public static async Task DiscoverNetworkAsync()
         {
@@ -26,13 +31,44 @@ namespace NodeControl
             await _xBee.DiscoverNetwork();
         }
 
-        static public event EventHandler InitializingController;
 
-        public static event EventHandler DiscoveringNetwork;
+        private static async Task Initialize()
+        {
+            if (!_isInitialized)
+            {
+                _xBee = await XBeeController.FindAndOpen(SerialPort.GetPortNames(), 9600);
+
+                if (_xBee == null)
+                    throw new InvalidOperationException("No XBee found.");
+
+#if TRACE
+                _xBee.FrameMemberDeserializing += XBeeOnFrameMemberDeserializing;
+                _xBee.FrameMemberDeserialized += XBeeOnFrameMemberDeserialized;
+#endif
+
+                _xBee.NodeDiscovered += XBeeOnNodeDiscovered;
+                _isInitialized = true;
+            }
+        }
+
+#if TRACE
+        private static void XBeeOnFrameMemberDeserializing(object sender, MemberSerializingEventArgs e)
+        {
+            var message = string.Format("D-Start: {0}", e.MemberName);
+            System.Diagnostics.Trace.WriteLine(message, "XBee");
+        }
+
+        private static void XBeeOnFrameMemberDeserialized(object sender, MemberSerializedEventArgs e)
+        {
+            var value = e.Value ?? "null";
+            var message = string.Format("D-End: {0} ({1})", e.MemberName, value);
+            System.Diagnostics.Trace.WriteLine(message, "XBee");
+        }
+#endif
 
         public static async void SetActivePorts(NodeAddress address, Ports ports)
         {
-            var node = await _xBee.GetRemoteAsync(address);
+            XBeeNode node = await _xBee.GetRemoteAsync(address);
 
             var gpioPorts = GpioPorts.None;
 
@@ -57,13 +93,13 @@ namespace NodeControl
             }
 
             var gpioWrite = new GpioWrite(gpioPorts);
-            
+
             await node.TransmitDataAsync(gpioWrite.GetPacket());
         }
 
         public static async Task SetNodeName(NodeAddress address, string name)
         {
-            var node = await _xBee.GetRemoteAsync(address);
+            XBeeNode node = await _xBee.GetRemoteAsync(address);
             await node.SetNodeIdentifier(name);
             await node.WriteChanges();
         }
@@ -90,27 +126,13 @@ namespace NodeControl
             }
         }
 
-        private async static Task Initialize()
-        {
-            if (!_isInitialized)
-            {
-                _xBee = await XBeeController.FindAndOpen(SerialPort.GetPortNames(), 9600);
-
-                if (_xBee == null)
-                    throw new InvalidOperationException("No XBee found.");
-
-                _xBee.NodeDiscovered += XBeeOnNodeDiscovered;
-                _isInitialized = true;
-            }
-        }
-
-        public static event EventHandler<NodeDiscoveredEventArgs> NodeDiscovered; 
+        public static event EventHandler<NodeDiscoveredEventArgs> NodeDiscovered;
 
         private static void XBeeOnNodeDiscovered(object sender, NodeDiscoveredEventArgs e)
         {
-            var node = e.Node;
+            XBeeNode node = e.Node;
 
-            if(NodeDiscovered != null)
+            if (NodeDiscovered != null)
                 NodeDiscovered(null, e);
 
             if (_xBee.HardwareVersion == HardwareVersion.XBeeSeries1 ||
